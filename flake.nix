4cfabff257fa5fa6d170984fa8b67c2c6832a4fc
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-05-19 17:12:02 +0200

    Configured nixpkgs to allow unfree packages

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
   let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs = 
+      import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
   in
   rec {
     packages.x86_64-linux.spacevim = import ./spacevim/default.nix {inherit pkgs; };
